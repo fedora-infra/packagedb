cd493296b0bc6201c0694df2b3e33df907675f89
Toshio Ernie Kuratomi <toshio@publictest1.fedora.phx.redhat.com>
2007-08-26 12:17:26 -0700

    * pkgdb/pkgdb/static/javascript/mochikit1.4.js: Split mochikit 1.4 functions   out of framework.js so we can clearly specify what's under the MochiKit   license and what's under our license. * README: Small update to status. * genacls.pkgdb, owner-sync-pkgdb, pkgdb-client, pkgdb-import.py,   pkgdb-login.py, pkgdb.sql, pkgdb/pkgdb/acls.py, pkgdb/pkgdb/collections.py,   pkgdb/pkgdb/controllers.py, pkgdb/pkgdb/dispatcher.py, pkgdb/pkgdb/json.py,   pkgdb/pkgdb/model.py, pkgdb/pkgdb/packages.py, pkgdb/pkgdb/release.py,   pkgdb/pkgdb/static/css/fedora.css, pkgdb/pkgdb/static/css/pkgdb.css,   pkgdb/pkgdb/static/css/style.css,   pkgdb/pkgdb/static/javascript/collectionoverview.js,   pkgdb/pkgdb/static/javascript/framework.js,   pkgdb/pkgdb/static/javascript/pkgpage.js,   pkgdb/pkgdb/static/javascript/tooltip.js, pkgdb/pkgdb/templates/layout.html,   pkgdb/pkgdb/templates/overview.html, pkgdb/pkgdb/users.py, pkgdb/setup.py,   pkgdb2branch.py: Add GPLv2 License and copyright notices .

--- pkgdb/pkgdb/release.py
+++ pkgdb/pkgdb/release.py
@@ -1,14 +1,19 @@
 # Release information about pkgdb
 
-version = "1.0"
+version = '0.3.1'
 
-# description = "Your plan to rule the world"
-# long_description = "More description about your plan"
-# author = "Your Name Here"
-# email = "YourEmail@YourDomain"
-# copyright = "Vintage 2006 - a good year indeed"
+description = 'The Fedora Package Database'
+long_description = '''
+Keep track of owner, maintainer, and parties interested in the development of
+a particular software package within Fedora.  This database is a central store
+for important package information nad allows querying of the package
+information from other tools.
+'''
+author = 'Toshio Kuratomi'
+email = 'tkuratom@redhat.com'
+copyright = '2007 Red Hat, Inc.'
 
 # if it's open source, you might want to specify these
-# url = "http://yourcool.site/"
-# download_url = "http://yourcool.site/download"
-# license = "MIT"
+url = 'https://admin.fedoraproject.org/pkgdb/'
+download_url = 'https://hosted.fedoraproject.org/projects/packagedb/'
+license = 'GPLv2'
