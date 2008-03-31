ce4f49ecf1367a9a8cb2edc18256cbad76da4d1a
John Palmieri (J5) <johnp@publictest10.fedoraproject.org>
2008-03-31 21:18:18 +0000

    Fix up for SA 0.4 upgrade

--- pkgdb/collections.py
+++ pkgdb/collections.py
@@ -22,9 +22,6 @@ Controller for showing Package Collections.
 '''
 
 import sqlalchemy
-### FIXME: Get rid of this with TurboGears 1.0.4
-from sqlalchemy.ext.selectresults import SelectResults
-import sqlalchemy.mods.selectresults
 
 from turbogears import controllers, expose, paginate, config, redirect
 from turbogears.database import session
@@ -45,7 +42,7 @@ class Collections(controllers.Controller):
     def index(self):
         '''List the Collections we know about.
         '''
-        collections = session.query(model.CollectionPackage).order_by(
+        collections = model.CollectionPackage.query.order_by(
                 (model.CollectionPackage.c.name,
                     model.CollectionPackage.c.version))
 
@@ -120,9 +117,9 @@ class Collections(controllers.Controller):
         #             model.PackageListing.c.collectionid==collectionId,
         #             model.PackageListing.c.packageid==model.Package.c.id)
         #         )
-        packages = SelectResults(session.query(model.Package)).select(
+        packages = model.Package.query.filter(
                 sqlalchemy.and_(model.PackageListing.c.collectionid==collectionId,
                     model.PackageListing.c.packageid==model.Package.c.id)
-                )
+                ).all()
         return dict(title='%s -- %s %s' % (self.appTitle, collection['name'],
             collection['version']), collection=collection, packages=packages)
