d34c66c9c842dee7afe1898b66f8e66edf9e69c8
Toshio Kuratomi <badger@Clingman>
2008-07-10 16:35:09 -0700

    Assign turbogears gettext builtin to a variable so we can import it normally.

--- pkgdb/__init__.py
+++ pkgdb/__init__.py
@@ -6,3 +6,10 @@ Fedora Collection.
 '''
 from pkgdb import release
 __version__ = release.VERSION
+
+# Assign a gettext function to "_" so that we can use it for i18n work.
+# Note: importing turbogears assigns this to a builtin _
+# pylint: disable-msg=W0611,E0601
+import turbogears
+_ = _
+# pylint: enable-msg=W0611,E0601
