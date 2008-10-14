8c856a4ffb20a7ac27e5b6be6e25cc25342d562a
Toshio Kuratomi <toshio@fedoraproject.org>
2008-10-14 16:30:28 -0700

    Add comment for why we're disabling certain pylint checks.

--- pkgdb/__init__.py
+++ pkgdb/__init__.py
@@ -8,8 +8,11 @@ from pkgdb import release
 __version__ = release.VERSION
 
 # Assign a gettext function to "_" so that we can use it for i18n work.
-# Note: importing turbogears assigns this to a builtin _
-# pylint: disable-msg=W0611,E0601
+# (W0611) we have to import turbogears so we get access to the builtin _
+# (W0622) _ is assigned to a builtin by turbogears but we want to be able to
+#   access it from code that might not import TurboGears
+# (E0601) _ is defined as a builtin by the turbogears import
+# pylint: disable-msg=W0611,W0622,E0601
 import turbogears
 _ = _
-# pylint: enable-msg=W0611,E0601
+# pylint: enable-msg=W0611,W0622,E0601
