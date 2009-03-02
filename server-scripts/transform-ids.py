343d40cd2b82aadc42e1b456db18480ee078d2d3
Ionuț Arțăriși <mapleoin@fp.o>
2009-03-02 18:07:28 +0200

    * server-scripts/transform-ids.py: made it generic again

--- server-scripts/transform-ids.py
+++ server-scripts/transform-ids.py
@@ -57,8 +57,8 @@ REVOKE UPDATE ON log FROM pkgdbadmin;
 '''
 
 # ugly configs so we can import tables from the model
-CONFDIR='/home/mapleoin/pkgdb' 
-PKGDBDIR=os.path.join('/home/mapleoin/pkgdb', 'fedora-packagedb') 
+CONFDIR='@CONFIDR@' 
+PKGDBDIR=os.path.join('@DATADIR', 'fedora-packagedb') 
 sys.path.append(PKGDBDIR) 
 
 if len(sys.argv) > 1:
