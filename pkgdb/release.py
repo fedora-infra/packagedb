752a5a6ec32073b3f84138e78fe55ed6ff2ccb32
Toshio Kuratomi <toshio@fedoraproject.org>
2009-05-13 00:14:32 -0700

    Mark translatable strings and fix a typo.

--- pkgdb/release.py
+++ pkgdb/release.py
@@ -1,16 +1,17 @@
 '''
 Information about this pkgdb release
 '''
+from pkgdb import _
 
 VERSION = '0.3.10.90'
 NAME = 'fedora-packagedb'
-DESCRIPTION = 'The Fedora Package Database'
-LONG_DESCRIPTION = '''
+DESCRIPTION = _('The Fedora Package Database')
+LONG_DESCRIPTION = _('''
 Keep track of owner, maintainer, and parties interested in the development of
 a particular software package within Fedora.  This database is a central store
-for important package information nad allows querying of the package
+for important package information and allows querying of the package
 information from other tools.
-'''
+''')
 AUTHOR = 'Toshio Kuratomi'
 EMAIL = 'toshio@fedoraproject.org'
 COPYRIGHT = '2007-2009 Red Hat, Inc.'
