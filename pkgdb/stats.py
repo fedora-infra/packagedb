0735b0273b077cc7d64d85da9d4aa1c238c1be18
Ionuț Arțăriși <mapleoin@fp.o>
2009-06-16 14:50:28 +0300

    small bugfixes

--- pkgdb/stats.py
+++ pkgdb/stats.py
@@ -104,10 +104,12 @@ class Stats(controllers.Controller):
             PackageListing.id).execute().rowcount
         # orphan packages in DEVEL 
         orphan_devel = PackageListing.query.filter_by(
-            status=STATUS['Orphaned'].statuscodeid, collectionid=DEVEL).count()
+            statuscode=STATUS['Orphaned'].statuscodeid, collectionid=DEVEL
+            ).count()
         # orphan packages in fedora 10
         orphan_latest = PackageListing.query.filter_by(
-            status=STATUS['Orphaned'].statuscodeid, collectionid=19).count()
+            statuscode=STATUS['Orphaned'].statuscodeid, collectionid=19
+            ).count()
 
         return dict(title=_('%(app)s -- Package Stats') % {
             'app': self.app_title},
