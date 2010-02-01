b69f26ca49fcae454a2076a1375b7652c77dfe38
Martin Bacovsky <mbacovsk@redhat.com>
2010-02-01 11:07:21 +0100

    Pakagebuilds list and search

--- pkgdb/builds.py
+++ pkgdb/builds.py
@@ -29,6 +29,7 @@ Controller for displaying PackageBuild related information
 
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.sql.expression import and_, literal_column, union
+from sqlalchemy import Text, Integer
 
 from turbogears import controllers, expose, identity, redirect, flash
 from turbogears import paginate
@@ -58,13 +59,123 @@ class BuildsController(controllers.Controller):
 
     @expose(template='pkgdb.templates.builds')
     def default(self):
-        '''Applications Home Page
-
-        This page serves shop-window of fedora applications.
-        Here you can see what's new and can search for applications
+        '''Builds Home Page
         '''
 
         redirect('/apps/name/list/a*')
+
+    
+    @expose(template='pkgdb.templates.builds_search')
+    @paginate('build_list', limit=50, default_order='name', max_limit=None,
+            max_pages=13) #pylint:disable-msg=C0322
+    def search(self, pattern=''):
+        '''Builds search result
+
+        :arg pattern: pattern to be looked for in apps.
+
+        Search is performed on name, license, architecture, version, release,
+        provides, requires, obsoletes, depends and files. Results are sorted according 
+        to relevancy. Parts where pattern was recognized are shown
+        in listing.
+        '''
+
+        pkg_list = []
+
+        if pattern == '':
+            flash('Insert search pattern...')
+
+        builds = self._builds_search_query(pattern).execute()
+
+        # merge all hits 
+        merged_results = {}
+        for b in builds:
+            result = merged_results.get(b['id'], None)
+            if result is None:
+                result = {
+                    'name': b['name'],
+                    'epoch': b['epoch'],
+                    'release': b['release'],
+                    'version': b['version'],
+                    'arch': b['architecture'],
+                    'repo': b['repo'],
+                    'score': 0,
+                    'obsoletes': [],
+                    'provides': [],
+                    'requires': [],
+                    'conflicts': [],
+                    'depend': [],
+                }
+            result['score'] += b['score']
+            if b['foundin'] == 'Obsoletes':
+                result['obsoletes'].append(b['data'])
+            elif b['foundin'] == 'Provides':
+                result['provides'].append(b['data'])
+            elif b['foundin'] == 'Requires':
+                result['requires'].append(b['data'])
+            elif b['foundin'] == 'Conflicts':
+                result['conflicts'].append(b['data'])
+            elif b['foundin'] == 'Depend':
+                result['depend'].append(b['data'])
+
+            merged_results[b['id']] = result
+
+        build_list = sorted(merged_results.values(), key=itemgetter('score'), reverse=True)
+                
+        return dict(title=self.app_title, version=release.VERSION,
+            pattern=pattern, build_list=build_list)
+
+
+    def _builds_search_query(self, pattern):
+        p = re.compile(r'\W+')
+        s_pattern = p.sub(' ', pattern).split(' ')
+
+        # name
+        q_name = session.query(
+                PackageBuild.id,
+                PackageBuild.name,
+                PackageBuild.epoch,
+                PackageBuild.version,
+                PackageBuild.release,
+                PackageBuild.architecture,
+                Repo.shortname.label('repo'),
+                literal_column("'Name'", Text).label('foundin'),
+                literal_column('100', Integer).label('score'),
+                literal_column('0', Integer).label('data'))\
+            .join(Repo)\
+            .filter(
+                and_(
+                    1 == 1, 
+                    *(PackageBuild.name.ilike('%%%s%%' % p) for p in s_pattern)
+                )
+            )
+
+        # union that
+        builds_query = union(
+                    q_name,
+                    )
+
+        return builds_query
+
+
+    @expose(template='pkgdb.templates.builds')
+    @paginate('build_list', limit=50, default_order='name', max_limit=None,
+            max_pages=13) #pylint:disable-msg=C0322
+    def name(self, searchwords='a*' ):
+        '''Builds view by name
+
+        :arg searchwords: filter used by letter_paginator
+        '''
+        
+        pattern = searchwords.replace('*','%')
+        #pylint:disable-msg=E1101
+        build_list = session.query(PackageBuild)\
+                .filter(
+                    PackageBuild.name.ilike(pattern))
+        #pylint:enable-msg=E1101
+
+        return dict(title=self.app_title, version=release.VERSION,
+            searchwords=searchwords, list_type='name',
+            build_list=build_list, pattern='')
 
 
     @expose(template='pkgdb.templates.build')
