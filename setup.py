6bed4580eba8095982f3634cb40b9d839850b2c2
Toshio Kuratomi <toshio@fedoraproject.org>
2012-10-08 08:57:46 -0700

    Some more fixes

--- setup.py
+++ setup.py
@@ -178,7 +178,7 @@ setup(
         "TurboGears[future] >= 1.0",
         "TurboMail",
         "python_fedora >= 0.3.12",
-        "SQLAlchemy >= 0.5.5",
+        "SQLAlchemy >= 0.5.5, < 0.7",
     ],
     scripts = ["start-pkgdb", "pkgdb.wsgi", "server-scripts/pkgdb-sync-yum",
         "server-scripts/pkgdb-sync-bugzilla", "clients/pkgdb-client",],
