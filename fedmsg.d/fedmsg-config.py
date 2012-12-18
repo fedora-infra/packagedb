fa94e11c7440b3e6c54df5081f025e05014c2832
Ralph Bean <rbean@redhat.com>
2012-12-18 18:13:10 -0500

    Pared down version of the example pkgdb fedmsg config.

--- fedmsg.d/fedmsg-config.py
+++ fedmsg.d/fedmsg-config.py
@@ -1,8 +1,8 @@
-""" This is an example configuration for fedmsg.
+""" This is an example pkgdb configuration for fedmsg.
+By convention, it is normally installed as ``/etc/fedmsg.d/pkgdb.py``
 
-For Fedora Infrastructure, this is kept in::
-
-    puppet/modules/fedmsg/templates/fedmsg.d/
+For Fedora Infrastructure, our own version of this file is kept in
+``puppet/modules/fedmsg/templates/fedmsg.d/``
 
 It needs to be globally available so remote consumers know how to find the
 pkgdb producer (wsgi process).
@@ -13,21 +13,8 @@ hostname = socket.gethostname()
 
 config = dict(
     endpoints={
-        "relay_outbound": ["tcp://127.0.0.1:4001"],
         "pkgdb.%s" % hostname: [
             "tcp://127.0.0.1:3005",
         ],
     },
-
-    relay_inbound="tcp://127.0.0.1:2003",
-    environment="dev",
-    high_water_mark=0,
-    io_threads=1,
-    post_init_sleep=0.2,
-    irc=[],
-    zmq_enabled=True,
-    zmq_strict=False,
-
-    sign_messages=False,
-    validate_messages=False,
 )
